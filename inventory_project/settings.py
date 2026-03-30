5cfba3bfcb21ebdb5b20bdc42ad8bab844ec6d7b
machelchimwaza04-sketch <machelchimwaza04@gmail.com>
2026-03-30 22:31:33 +0100

    added product model and admin panel

--- inventory_project/settings.py
+++ inventory_project/settings.py
@@ -32,11 +32,13 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'django.contrib.admin',
-    'django.contrib.auth',
+    'django.contrib.auth',  
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'products',
+
 ]
 
 MIDDLEWARE = [
